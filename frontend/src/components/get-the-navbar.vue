3bb72bcbe56687171339e41652c0e8d8ad4778ff
lucas625 <lagc@cin.ufpe.br>
2020-05-09 18:21:28 -0300

    starting sign in view

---
+++ frontend/src/components/get-the-navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-card>
+    <v-toolbar
+      color="blue accent-1"
+    >
+      <v-app-bar-nav-icon />
+
+      <v-toolbar-title>GetOn-RPG</v-toolbar-title>
+
+      <v-spacer />
+
+      <get-router-icon
+        icon-name="mdi-home"
+        url="/"
+      />
+
+      <get-router-icon
+        icon-name="mdi-account-circle"
+        url="/sign-in"
+      />
+    </v-toolbar>
+  </v-card>
+</template>
+
+<script>
+import GetRouterIcon from '@/components/get-router-icon'
+
+export default {
+  name: 'GetNavbar',
+  components: {
+    GetRouterIcon
+  }
+}
+</script>
